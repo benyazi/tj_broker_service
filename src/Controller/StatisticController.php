62f109931a3250ca673fd67ee8850a7b9e08a97d
Sergey Klabukov <benjamin.yazi@gmail.com>
2019-10-22 00:51:38 +0200

    added broker graphs

---
+++ src/Controller/StatisticController.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\StockPrice;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class StatisticController extends AbstractController
+{
+    /** @var EntityManager */
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/statistic/graphs")
+     */
+    public function graphAction(Request $request)
+    {
+        $dt = (new \DateTime())->sub(new \DateInterval('P7D'));
+        $prices = $this->em->getRepository(StockPrice::class)
+            ->createQueryBuilder('p')
+            ->andWhere('p.priceDate > :dt')->setParameter('dt', $dt)
+            ->addOrderBy('p.publicOfferingId', 'ASC')
+            ->addOrderBy('p.priceDate', 'ASC')
+            ->getQuery()->getResult();
+        $dates = [];
+        $datasets = [];
+        /** @var StockPrice $price */
+        foreach ($prices as $price)
+        {
+            if($price->getOldKarma() < 1) {
+                continue;
+            }
+            $value = $price->getCurrentKarma() - $price->getOldKarma();
+            if(!isset($dates[$price->getPriceDate()->format('d.m H')])) {
+                $dates[$price->getPriceDate()->format('d.m H')] = 1;
+            }
+            if(!isset($datasets[$price->getPublicOfferingId()])) {
+                $datasets[$price->getPublicOfferingId()] = [
+                    'label' => $price->getPublicOffering()->getTitle(),
+                    'data' => [],
+//                    'backgroundColor' => 'red',
+                    'borderColor' => '#'.$this->random_color(),
+                    'borderWidth' => 1
+                ];
+            }
+            $datasets[$price->getPublicOfferingId()]['data'][] = $value;
+        }
+
+        return $this->render('statistic/index.html.twig', [
+            'dates' => array_keys($dates),
+            'datasets' => array_values($datasets)
+        ]);
+    }
+
+    private function random_color_part() {
+        return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+    }
+
+    private function random_color() {
+        return $this->random_color_part() . $this->random_color_part() . $this->random_color_part();
+    }
+}
